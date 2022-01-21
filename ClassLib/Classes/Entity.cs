2b6493fc918431eb25ac7feaee28e4741d73c92b
Cezary Szczepański <cezary.szczepanski99@gmail.com>
2022-01-21 13:16:34 +0100

    Entity class declaraction

--- ClassLib/Classes/Entity.cs
+++ ClassLib/Classes/Entity.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ClassLib.Classes
+﻿namespace ClassLib.Classes
 {
     public class Entity
     {
+        private string _name { get; set; } = "no_name";
+        private double _healthPoints { get; set; } = 0;
+        private int _gold { get; set; } = 0;
+
+        #region staty
+        private int _strength { get; set; } = 1;
+        private int _dexterity { get; set; } = 1;
+        private int _intelligence { get; set; } = 1;
+        private int _charisma { get; set; } = 1;
+        private int _endurance { get; set; } = 1;
+        private double _maxHealthPoints { get; set; } = 1;
+
+        #endregion
+        private int test;
+        public Entity(string name, int gold, int strength, int dexterity, int intelligence, int charisma, int endurance, double maxHealthPoints)
+        {
+            if (name != null)
+            {
+                this._name = name;
+            }
+            if (maxHealthPoints > 0)
+            {
+                this._maxHealthPoints = maxHealthPoints;
+                this._healthPoints = maxHealthPoints;
+            }
+
+            int[] statTable = new int[5] { strength, dexterity, intelligence, charisma, endurance };
+            int[] _statTable = new int[5] { _strength, _dexterity, _intelligence, _charisma, _endurance };
+
+            for (int i = 0; i < statTable.Length; i++)
+            {
+                if (statTable[i] > 0)
+                {
+                    _statTable[i] = statTable[i];
+                    //cimcirymci
+                }
+            }
+        }
     }
 }
